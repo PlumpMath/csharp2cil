a87f8b9c30602664a6fd2999339b233ed9bf7945
vkairys <kairys.vilius@gmail.com>
2015-02-11 20:25:40 +0200

    refactor js

--- src/CSharp2CIL/Scripts/csharp2cil.js
+++ src/CSharp2CIL/Scripts/csharp2cil.js
@@ -1,107 +1,96 @@
-﻿var editor = CodeMirror.fromTextArea(document.getElementById('csharp'), {
-    mode: "text/x-csharp",
+﻿var csharpEditor = CodeMirror.fromTextArea(document.getElementById('csharp'), {
+    mode: 'text/x-csharp',
     lineNumbers: true
 });
-editor.setSize(null, '97%');
 
-var editor2 = CodeMirror.fromTextArea(document.getElementById('cil'), {
+var cilEditor = CodeMirror.fromTextArea(document.getElementById('cil'), {
     lineNumbers: true
 });
-editor2.setSize(null, '97%');
 
-var cilLines = [];
-function parse() {
-    $.post('/Home/Parse', 'csCode=' + encodeURIComponent(editor.getValue()), function (types) {
-        if (types !== "error") {
-            var text = "";
-            var lineNumber = 0;
-            for (var i = 0; i < types.length; i++) {
-                var type = types[i];
-                csharpClassDeclarationLines = [type.StartLine + 1, type.EndLine + 1];
-                cilLines[lineNumber++] = csharpClassDeclarationLines; //class name
-                cilLines[lineNumber++] = csharpClassDeclarationLines;
-                text += '.class ' + type.Name + '\n';
-                text += '{\n';
+var editorLines = [];
 
-                //methods
-                type.CilMethods.forEach(function (method) {
-                    var methodDeclarationLines = [method.StartLine + 1, method.EndLine + 1];
-                    cilLines[lineNumber++] = methodDeclarationLines;
-                    cilLines[lineNumber++] = methodDeclarationLines;
-                    text += '    .method ' + method.Name + '\n';
-                    text += '    {\n';
-                    //body
-                    method.CilLineInsturctions.forEach(function (block) {
-                        block.Instructions.forEach(function (instruction) {
-                            text += '     ' + instruction + '\n';
-                            cilLines[lineNumber++] = [block.Line];
-                        });
-                    });
+function setEditorText(cilTypes) {
+    var text = '';
+    cilTypes.forEach(function (type) {
+        text += '.class ' + type.Name + '\n';
+        text += '{\n';
+        editorLines.push([type.StartLine, type.EndLine]);
+        editorLines.push([type.StartLine, type.EndLine]);
 
-                    cilLines[lineNumber++] = methodDeclarationLines;
-                    text += '    }\n';
+        type.CilMethods.forEach(function (method) {
+            text += '    .method ' + method.Name + '\n';
+            text += '    {\n';
+            editorLines.push([method.StartLine, method.EndLine]);
+            editorLines.push([method.StartLine, method.EndLine]);
+
+            method.CilLineInsturctions.forEach(function (block) {
+                block.Instructions.forEach(function (instruction) {
+                    text += '     ' + instruction + '\n';
+                    editorLines.push([block.Line - 1]);
                 });
-
-                cilLines[lineNumber++] = csharpClassDeclarationLines;
-                text += '}\n';
-            }
-
-            editor2.setValue(text);
-
-
-            //-------------------- handler to csharp code
-            var hovered = [];
-            var currentHover;
-            $('>', $('.CodeMirror-code').eq(0)).hover(function () {
-
-                var lineNumber = parseInt(this.innerText);
-                for (var i = 0; i < cilLines.length; i++) {
-                    if (cilLines[i].indexOf(parseInt(lineNumber)) != -1) {
-                        var line = $('> div:nth-child(' + (i + 1) + ')', $('.CodeMirror-code').eq(1));
-                        line.css({ 'background-color': '#00a855' });
-                        hovered.push(line);
-                    }
-
-                }
-                currentHover = $('> div:nth-child(' + lineNumber + ')', $('.CodeMirror-code').eq(0));
-                currentHover.css({ 'background-color': '#00a855' });
-
-
-            }, function () {
-                hovered.forEach(function(line) {
-                    line.css({ 'background-color': 'white' });
-                });
-                hovered = [];
-                currentHover.css({ 'background-color': 'white' });
             });
 
+            text += '    }\n';
+            editorLines.push([method.StartLine, method.EndLine]);
+        });
 
-            //----------handler to cil code
-            //var hovered = [];
-            $('>', $('.CodeMirror-code').eq(1)).hover(function () {
-                var lineNumber = parseInt(this.innerText) - 1;
-                if (cilLines[lineNumber] !== undefined) {
-                    cilLines[lineNumber].forEach(function (csharpLine) {
-                        var line = $('> div:nth-child(' + csharpLine + ')', $('.CodeMirror-code').eq(0));
-                        line.css({ 'background-color': '#00a855' });
-                        hovered.push(line);
-                    })
-                    currentHover = $('> div:nth-child(' + (lineNumber + 1) + ')', $('.CodeMirror-code').eq(1));
-                    currentHover.css({ 'background-color': '#00a855' });
-
-                }
-
-            }, function () {
-                hovered.forEach(function (line) {
-                    line.css({ 'background-color': 'white' });
-                });
-                hovered = [];
-                currentHover.css({ 'background-color': 'white' });
-            });
-        } else {
-            editor2.setValue('error');
-        }
-
+        text += '}\n';
+        editorLines.push([type.StartLine, type.EndLine]);
     });
 
+    cilEditor.setValue(text);
+}
+
+function setHandlers() {
+    var highlightedLines = [];
+    var csharpLines = $('.CodeMirror-code').eq(0).children();
+    var cilLines = $('.CodeMirror-code').eq(1).children();
+
+    function highlight(line) {
+        line.css({ 'background-color': '#00a855' });
+        highlightedLines.push(line);
+    }
+
+    function removeHighlights() {
+        highlightedLines.forEach(function (line) {
+            line.css({ 'background-color': 'white' });
+        });
+        highlightedLines = [];
+    }
+
+    csharpLines.hover(function () {
+        var currentHover = $(this);
+        var lineNumber = currentHover.index();
+        highlight(currentHover);
+
+        editorLines.forEach(function (editorLine, i) {
+            if (editorLine.indexOf(lineNumber) !== -1) {
+                highlight(cilLines.eq(i));
+            }
+        });
+    }, removeHighlights);
+
+    cilLines.hover(function () {
+        var currentHover = $(this);
+        var lineNumber = currentHover.index();
+        highlight(currentHover);
+
+        if (editorLines[lineNumber] !== undefined) {
+            editorLines[lineNumber].forEach(function (editorLine) {
+                highlight(csharpLines.eq(editorLine));
+            });
+        };
+    }, removeHighlights);
+}
+
+function parse() {
+    $.post('/Home/Parse', 'csCode=' + encodeURIComponent(csharpEditor.getValue()), function (types) {
+        if (types !== 'error') {
+            editorLines = [];
+            setEditorText(types);
+            setHandlers();
+        } else {
+            cilEditor.setValue('error');
+        }
+    });
 }
