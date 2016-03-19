52c3544f7bbba327c232bddbaf3b28564ce48687
Vilius Kairys <kairys.vilius@gmail.com>
2016-03-19 16:48:50 +0200

    moved declaration parsing

--- src/CSharp2CIL/Controllers/HomeController.cs
+++ src/CSharp2CIL/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Web.Mvc;
 using Microsoft.CodeAnalysis;
+using Mono.Cecil.Cil;
 
 namespace CSharp2CIL.Controllers
 {
@@ -23,28 +24,6 @@ namespace CSharp2CIL.Controllers
             var tree = CSharpSyntaxTree.ParseText(csCode);
             if (!tree.GetDiagnostics().Any(d => d.IsWarningAsError))
             {
-                var csTypes = tree.GetRoot().DescendantNodes().OfType<TypeDeclarationSyntax>();
-
-                var cilTypes = csTypes.Select(type => new CSharp2CIL.Models.Type
-                {
-                    Name = type.Identifier.ToString(),
-                    Lines = new[]{
-                        type.GetLocation().GetLineSpan().StartLinePosition.Line,
-                        type.OpenBraceToken.GetLocation().GetLineSpan().StartLinePosition.Line,
-                        type.CloseBraceToken.GetLocation().GetLineSpan().StartLinePosition.Line
-                    },
-                    Methods = type.Members.OfType<MethodDeclarationSyntax>().Select(method => new Method
-                    {
-                        Name = method.Identifier.ToString(),
-                        Lines = new[]{
-                            method.GetLocation().GetLineSpan().StartLinePosition.Line,
-                            method.Body.OpenBraceToken.GetLocation().GetLineSpan().StartLinePosition.Line,
-                            method.Body.CloseBraceToken.GetLocation().GetLineSpan().StartLinePosition.Line
-                        }
-                    }).ToList()
-                }).ToList();
-
-
                 var compilation = CSharpCompilation.Create("temp", new[] { tree },
                     new[] { MetadataReference.CreateFromAssembly(typeof(object).Assembly) }, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
                 var stream = new MemoryStream();
@@ -53,6 +32,7 @@ namespace CSharp2CIL.Controllers
 
                 if (result.Success)
                 {
+                    var declarations = ParseDeclarations(tree.GetRoot().DescendantNodes().OfType<TypeDeclarationSyntax>());
                     stream.Seek(0, SeekOrigin.Begin);
 
                     var readerParameters = new ReaderParameters { ReadSymbols = true, SymbolStream = pdbStream };
@@ -60,7 +40,7 @@ namespace CSharp2CIL.Controllers
 
                     foreach (var cilType in assemblyDefinition.MainModule.Types)
                     {
-                        var codeType = cilTypes.FirstOrDefault(t => t.Name == cilType.Name);
+                        var codeType = declarations.FirstOrDefault(t => t.Name == cilType.Name);
                         if (codeType != null)
                         {
                             foreach (var cilMethod in cilType.Methods)
@@ -68,30 +48,72 @@ namespace CSharp2CIL.Controllers
                                 var codeMethod = codeType.Methods.FirstOrDefault(m => m.Name == cilMethod.Name);
                                 if (codeMethod != null)
                                 {
-                                    codeMethod.BodyLines = new List<BodyLine>();
-                                    BodyLine bodyLine = null;
-                                    foreach (var instruction in cilMethod.Body.Instructions)
-                                    {
-                                        if (instruction.SequencePoint != null && instruction.SequencePoint.StartLine < 100) //new code line
-                                        {
-                                            bodyLine = new BodyLine
-                                            {
-                                                Line = instruction.SequencePoint.StartLine,
-                                                Instructions = new List<string>()
-                                            };
-                                            codeMethod.BodyLines.Add(bodyLine);
-                                        }
-                                        bodyLine.Instructions.Add(instruction.ToString());
-                                    }
+                                    codeMethod.BodyLines = ParseInstructions(cilMethod.Body.Instructions);
                                 }
                             }
                         }
                     }
-                    return Json(cilTypes);
+                    return Json(declarations);
                 }
             }
 
             return Json("error");
         }
+
+        private IEnumerable<CSharp2CIL.Models.Type> ParseDeclarations(IEnumerable<TypeDeclarationSyntax> typeDeclarations)
+        {
+            var types = new List<CSharp2CIL.Models.Type>();
+            foreach (var type in typeDeclarations)
+            {
+                var cilType = new CSharp2CIL.Models.Type
+                {
+                    Name = type.Identifier.ToString(),
+                    Lines = new[]
+                    {
+                        type.GetLocation().GetLineSpan().StartLinePosition.Line,
+                        type.OpenBraceToken.GetLocation().GetLineSpan().StartLinePosition.Line,
+                        type.CloseBraceToken.GetLocation().GetLineSpan().StartLinePosition.Line
+                    }
+                };
+                foreach (var method in type.Members.OfType<MethodDeclarationSyntax>())
+                {
+                    cilType.Methods.Add(new Method
+                    {
+                        Name = method.Identifier.ToString(),
+                        Lines = new[]
+                        {
+                            method.GetLocation().GetLineSpan().StartLinePosition.Line,
+                            method.Body.OpenBraceToken.GetLocation().GetLineSpan().StartLinePosition.Line,
+                            method.Body.CloseBraceToken.GetLocation().GetLineSpan().StartLinePosition.Line
+                        }
+                    });
+                }
+                types.Add(cilType);
+            }
+
+            return types;
+        }
+
+        private List<BodyLine> ParseInstructions(IEnumerable<Instruction> instructions)
+        {
+            var lines = new List<BodyLine>();
+            BodyLine line = null;
+            foreach (var instruction in instructions)
+            {
+                if (instruction.SequencePoint != null && instruction.SequencePoint.StartLine < 100) //new code line
+                {
+                    line = new BodyLine
+                    {
+                        Line = instruction.SequencePoint.StartLine,
+                        Instructions = new List<string>()
+                    };
+                    lines.Add(line);
+                }
+                line.Instructions.Add(instruction.ToString());
+            }
+
+            return lines;
+        }
+
     }
 }
